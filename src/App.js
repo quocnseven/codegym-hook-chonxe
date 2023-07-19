6567d0be41585abe05042f9f434d4f2d1f93a814
Quoc Nguyen <quocng1997@gmail.com>
2023-07-19 18:46:30 +0700

    them bai lam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState("")
+  const [state2, setState2] = useState("")
+  
+
+  const handleCar = event => {
+    setState(event.target.value)
+    
+  }
+
+  const handleColor = event => {
+    setState2(event.target.value)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center", padding: 30, backgroundColor: "gold"}}>
+      <form>
+        <select onChange={handleCar} id="cars" name="cars">
+          <option value="Volvo">Volvo</option>
+          <option value="Saab">Saab</option>
+          <option value="Fiat">Fiat</option>
+          <option value="Audi">Audi</option>
+        </select>
+
+        
+        <select onChange={handleColor} id="colors" name="colors">
+          <option value="Yellow">Yellow</option>
+          <option value="Black">Black</option>
+          <option value="White">White</option>
+          <option value="Gold">Gold</option>
+        </select>
+        <p>Bạn đã chọn xe {state}, màu {state2}</p>
+      </form>
+      
     </div>
-  );
+  )
 }
 
 export default App;
